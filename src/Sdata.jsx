f99b67a451ed3db4985391786986273c3fb49d41
karanbisht545 <karanbisht844@gmail.com>
2022-05-09 00:19:11 +0530

    df

---
+++ src/Sdata.jsx
@@ -0,0 +1,19 @@
+const Sdata=[
+    {
+        imgsrc:"https://wallpapercave.com/wp/wp4056410.jpg",
+        sname:"DARK",
+        title:"A Netflix Original Series",
+        link:"https://www.netflix.com/in/title/80100172",
+
+    },
+    {
+        imgsrc:"https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg" , title:"A Netflix Original Series", sname:"EXTRA CURRICULAR",
+        link:"https://www.netflix.com/in/title/80990668?source=35"
+    },
+    {
+        imgsrc:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhJmzlKuVKhIz9hsStEwi8Yc7qA9XzYXbAKg&usqp=CAU" ,
+        title:"A Netflix Original Series", sname:"OZARK",
+       link:"https://www.netflix.com/in/title/80117552",
+    },
+];
+export default Sdata;
