7491e8eaa24ab04a242f728021a154907a3e90fc
karanbisht545 <karanbisht844@gmail.com>
2022-05-09 00:16:16 +0530

    dflj

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Card from './components/Card';
+import Sdata from './components/Sdata';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return ( 
+    <>
+    <h1 className='heading_style'>List of top 5 Netflix series in 2020</h1>
+    <Card
+      imgsrc={Sdata[0].imgsrc}
+      title={Sdata[0].title}
+      sname={Sdata[0].sname}
+      link={Sdata[0].link}
+    /> 
+    <Card
+      imgsrc={Sdata[1].imgsrc}
+      title={Sdata[1].title}
+      sname={Sdata[1].sname}
+      link={Sdata[1].link}
+    />
+     <Card
+      imgsrc={Sdata[2].imgsrc}
+      title={Sdata[2].title}
+      sname={Sdata[2].sname}
+      link={Sdata[2].link}
+    />
+    </>
+);
 }
 
 export default App;
